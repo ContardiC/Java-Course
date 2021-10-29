3d3dc683a48b129fb1061cab66a11d6f16fdd70e
Carlo Contardi <c.contardi@icloud.com>
2021-10-29 13:06:26 +0200

    SetNumeri added

---
+++ src/exercises/SetNumeri.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class SetNumeri {
+    // attributi
+    double numeri[];
+    // costruttori
+    SetNumeri(int n){
+        numeri=new double[n];
+        inserimento();
+    }
+    SetNumeri(double numeri[]){
+        this.numeri=numeri;
+    }
+    void inserimento(){
+       for(int i=0;i<numeri.length;i++) {
+            // input std input
+       }
+    }
+}
